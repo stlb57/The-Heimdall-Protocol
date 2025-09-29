38c7c9aa980c8d0947a5e091f6adea3f6448ece8
stlb57 <saarthaktyagi1@gmail.com>
2025-09-29 07:41:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,30 +6,25 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_instance" "drone_server" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
+  key_name      = "heimdall-key"
+
   user_data = <<-EOF
               #!/bin/bash
-              # Update the package list
               sudo apt-get update -y
-              
-              # Install Docker
               sudo apt-get install -y docker.io
-              
-              # Start the Docker service
               sudo systemctl start docker
-              
-              # Enable Docker to start on boot
               sudo systemctl enable docker
-              
-              # Add the 'ubuntu' user to the 'docker' group so we can run
-              # Docker commands without sudo. This is critical for Jenkins.
               sudo usermod -aG docker ubuntu
               EOF
+
   tags = {
     Name = "Heimdall-Protocol-Drone"
   }
